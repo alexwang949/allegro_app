4eb514ad044102cbdaee7dd311099aa2aaa20d82
alexwang949 <alex_cellist@yahoo.com>
2015-02-24 12:38:08 -0500

    added $autoScroll onclick for events, added delete function to favs

--- public/js/script.js
+++ public/js/script.js
@@ -1,170 +1,183 @@
 console.log('linked!');
-window.onload = function () {
-var $categButton = $('.categ-button');
-var $videoArea = $('.video-area');
-var $eventsArea = $('.events-area');
-var $myEventsButton = $('#my-events-button');
-// var $eventsList = $('.ul-area');
+// window.onload = function () {
 
-function getPieces(id) {
-	
-	$.ajax({
-		url: '/moods/' + id,
-		method: 'GET',
-		datatype: 'json'
-	}).done(function(data){
-		var parsed = JSON.parse(data);
-		var first = parsed[0];
-		// console.log(first.url, first.title, first.url);
-		$('.description-area').empty();
-		$('.description-area').append('<h5>Composer:</h5> ' + first.composer + '<h5>Title:</h5>' + first.title + '<h5>History:</h5>' + first.info);
-		$videoArea.append('<iframe id="ytplayer" type="text/html" width="500px" height="290px"src=' + first.url + 'modestbranding=1&autohide=1&showinfo=0&controls=0&autoplay=1frameborder="20"/>');
+	var $categButton = $('.categ-button');
+	var $videoArea = $('.video-area');
+	var $eventsArea = $('.events-area');
+	var $myEventsButton = $('#my-events-button');
 
+	function getPieces(id) {
+		
+		$.ajax({
+			url: '/moods/' + id,
+			method: 'GET',
+			datatype: 'json'
+		}).done(function(data){
+			var parsed = JSON.parse(data);
+			var first = parsed[0];
+			$('.description-area').empty();
+			$('.description-area').append('<h5>Composer:</h5> ' + first.composer + '<h5>Title:</h5>' + first.title + '<h5>History:</h5>' + first.info);
+			$videoArea.append('<iframe id="ytplayer" type="text/html" width="500px" height="290px"src=' + first.url + 'modestbranding=1&autohide=1&showinfo=0&controls=0&autoplay=1frameborder="20"/>');
+
+		});
+	};
+
+	$categButton.on('click', function() {
+		
+		$videoArea.empty();
+		var moodId = $(this).attr('id');
+		getPieces(moodId);
 	});
-};
-
-$categButton.on('click', function() {
-	
-	$videoArea.empty();
-	var moodId = $(this).attr('id');
-	getPieces(moodId);
-});
 
 
-//*****SHOW ALL EVENTS ***********************
+	$('#events-button').on('click', function(){
+		getEventsApi();
+		getEventsDB();
+	});
 
-$('#events-button').click(function(){
-	getEventsApi();
-	getEventsDB();
-})
+	$myEventsButton.on('click', function(){
+		$eventsArea.empty();
 
-$myEventsButton.on('click', function(){
-	$eventsArea.empty();
-
-	$.ajax({
-		url: '/favorites/user',
-		method: 'GET',
-		datatype: 'json'
-	}).done(function(data){
-		var parsed = JSON.parse(data);
-		populateMyFavEventsDOM(parsed);
-	})
-})
-
-
-function populateMyFavEventsDOM(parsed) {
-
-	parsed.forEach(function(each){
-
-	var $li = $('<br><h3>Event:</h3>' + each.event_name + '<h3>When:</h3>' + each.date_time_description + '<h3>Location:</h3>' + each.street_address + '<h3>Neighborhood:</h3>' + each.neighborhood + '<h3>Description:</h3>' + each.web_description);
-
-	// var $li = $('<h3>Event:</h3>' + each.event_name +' ' + '<h3>Location</h1>' + each.street_address + '<h3>Neighborhood:</h3>' + each.neighborhood + each.web_description + each.date_time_description);
-
-	$eventsArea.append($li);
-
-	})
-}
+		$.ajax({
+			url: '/favorites/user',
+			method: 'GET',
+			datatype: 'json'
+		}).done(function(data){
+			var parsed = JSON.parse(data);
+			populateMyFavEventsDOM(parsed);
+		});
+		scrollTest();
+	});
 
 
+	function populateMyFavEventsDOM(parsed) {
 
+		parsed.forEach(function(each){
 
-function populateEventsDOM(data) {
-	
+			var $div = $("<div>");
+			var $deleteFavoritesButton = $('<button class="button-primary" id="deleteFavoritesButton">REMOVE</button>');
+			var $event_id = each.id
+			var $li = $('<br><h3>Event:</h3>' + each.event_name + '<h3>When:</h3>' + each.date_time_description + '<h3>Location:</h3>' + each.street_address + '<h3>Neighborhood:</h3>' + each.neighborhood + '<h3>Description:</h3>' + each.web_description);
 
-	data.forEach(function(each){
+			$div.attr('event_id', $event_id);
+
+			$deleteFavoritesButton.on("click", function(){
+				deleteFavoriteEvent($event_id);
+				$(this).parent('div').remove();
+			});
+
+			$div.append($li);
+			$div.append($deleteFavoritesButton);
+			$div.append('<hr>')
+			$eventsArea.append($div);
+
+		});
+	}
+
+	function populateEventsDOM(data) {
+
+		data.forEach(function(each){
 
 			var $testDiv = $("<div>");
 			var $li = $('<br><h3>Event:</h3>' + each.event_name + '<h3>When:</h3>' + each.date_time_description + '<h3>Location:</h3>' + each.street_address + '<h3>Neighborhood:</h3>' + each.neighborhood + '<h3>Description:</h3>' + each.web_description);
-			// debugger
 			var $addToFavoritesButton = $('<button class="button-primary" id="addToFavoritesButton">ADD TO FAVORITES</button><hr>');
-			
-			// $li.attr('api_id', each.id);
-			// $eventsArea.append('<br><h1>Event:</h1>' + each.event_name +' ' + '<h1>Location</h1>' + each.street_address + '<h1>Neighborhood:</h1>' + each.neighborhood + each.web_description + each.date_time_description + "<br><button id='add-to-favorites'>ADD TO FAVORITES</button><br><hr>")
+			// var $deleteFavoritesButton = $('<button class="button-primary" id="deleteFavoritesButton">REMOVE</button>');
+			// var $event_id = each.id
+
+			// $deleteFavoritesButton.on("click", function(){
+			// 	deleteFavoriteEvent($event_id);
+			// });
 			$addToFavoritesButton.on("click", function(){
-					event_id_hash = {event_id: each.id}
-					addFavoriteEvent(event_id_hash);
-					// addFavoriteEvent(event_id_hash);
+				event_id_hash = {event_id: each.id};
+				addFavoriteEvent(event_id_hash);
 			});
 
-			$testDiv.append($li)
-			$testDiv.append($addToFavoritesButton)
-			// $li.append($addToFavoritesButton);
+			$testDiv.append($li);
+			// $testDiv.attr('event_id', $event_id);
+			$testDiv.append($addToFavoritesButton);
 			$eventsArea.append($testDiv);
-	});
-};
-// };
+		});
+		scrollTest();
+	};
 
-function getEventsDB() {
+	function getEventsDB() {
 
-	console.log('getEventsDB hit!');
+		console.log('getEventsDB hit!');
 
-	// var $addToFavoritesButton = $('#add-to-favorites');
-	$eventsArea.empty();
+		$eventsArea.empty();
 
-	$.ajax({
-		url: '/eventsdb',
-		method: 'GET',
-		datatype: 'json'
-	}).done(function(data){
-		var parsed = JSON.parse(data);
-		populateEventsDOM(parsed);
+		$.ajax({
+			url: '/eventsdb',
+			method: 'GET',
+			datatype: 'json'
+		}).done(function(data){
+			var parsed = JSON.parse(data);
+			populateEventsDOM(parsed);
 
-	});
-};
-
-
-function getEventsApi() {
-
-	console.log('getEventsApi hit!');
-
-	$.ajax({
-		url: '/events',
-		method: 'GET',
-		datatype: 'json'
-	}).done(function(data){
-		console.log(data);
-	});
-};
-
-function getMyEvents() {
-
-	console.log('getMyEvents hit!');
-
-	$eventsArea.empty();
-
-	$.ajax({
-		url: '/favorites/user',
-		method: 'GET',
-		datatype: 'json'
-	}).done(function(data){
-		console.log(data);
-	});
-};
-
-// $addToFavoritesButton.on('click', function() {
+		});
+	};
 
 
-// })
+	function getEventsApi() {
+
+		console.log('getEventsApi hit!');
+
+		$.ajax({
+			url: '/events',
+			method: 'GET',
+			datatype: 'json'
+		}).done(function(data){
+			console.log(data);
+		});
+	};
+
+	function getMyEvents() {
+
+		console.log('getMyEvents hit!');
+
+		$eventsArea.empty();
+
+		$.ajax({
+			url: '/favorites/user',
+			method: 'GET',
+			datatype: 'json'
+		}).done(function(data){
+			console.log(data);
+		});
+	}
 
 
-function addFavoriteEvent(event_id_hash) {
+	function addFavoriteEvent(event_id_hash) {
 
-	console.log('addFavoriteEvent hit!');
+		console.log('addFavoriteEvent hit!');
 
-	$.ajax({
-		url: '/favorites',
-		method: 'POST',
-		datatype: 'json',
-		data: event_id_hash
-	}).done(function(data){
-		console.log(data);
-	});
-};
+		$.ajax({
+			url: '/favorites',
+			method: 'POST',
+			datatype: 'json',
+			data: event_id_hash
+		}).done(function(data){
+			console.log(data);
+		});
+	};
 
+	function deleteFavoriteEvent(id) {
 
+		$.ajax({
+			url: '/favorites/' + id,
+			method: 'DELETE',
+			datatype: 'json'
+		}).done(function(data){
+			console.log('deleted!');
+		});
+	};
 
+	function scrollTest() {
+		$('body').animate({scrollTop: 480}, 'slow');
+		var scrolled = $(window).scrollTop();
+	}
 
-}
+// }
 
 
 
--- server.rb
+++ server.rb
@@ -3,9 +3,9 @@ require 'sinatra/reloader'
 require './lib/mood.rb'
 require './lib/piece.rb'
 require './lib/user.rb'
-# require './lib/connection.rb'
+require './lib/connection.rb'
 # require './lib/database.yml'
-require './environments.rb'
+# require './environments.rb'
 require './lib/event.rb'
 require './lib/favorite.rb'
 require 'sinatra'
@@ -32,7 +32,6 @@ end
 # login 
 post '/login' do
 	current_user = User.find_by(username: params[:username])
-
 	if current_user && current_user["password"] == params["password"]
 		session["user_id"] = current_user["id"]
 		redirect('/welcome')
@@ -56,16 +55,13 @@ get '/moods/:id' do
 	pieces = Piece.where(mood_id: params[:id]).to_json
 end
 
-
 # API CALL EVENTS, CONDITIONAL TO CHECK IF EVENT EXISTS.
 # 	IF DOESN'T EXIST, ADD TO EVENTS TABLE
 
 get '/events' do 
-	# response = JSON.generate(HTTParty.get('http://api.nytimes.com/svc/events/v2/listings.json?&filters=category:Classical&limit=5&api-key=bf3013d600013e74c7e448bff56e2f1f:18:70373815'))
 	response = HTTParty.get('http://api.nytimes.com/svc/events/v2/listings.json?&filters=category:Classical&limit=5&api-key=bf3013d600013e74c7e448bff56e2f1f:18:70373815')
 	results = response['results']
 	results.each do |event|
-
 		if Event.find_by(event_api_id: event['event_id']) == nil
 			event = Event.create({event_name: event['event_name'], venue_name: event['venue_name'], neighborhood: event['neighborhood'], street_address: event['street_address'], telephone: event['telephone'], web_description: event['web_description'], postal_code: event['postal_code'], venue_detail_url: event['venue_detail_url'], date_time_description: event['date_time_description'], event_api_id: event['event_id']})
 		end
@@ -74,7 +70,6 @@ get '/events' do
 end
 
 get '/eventsdb' do 
-	# binding.pry
 	events = Event.all.to_json
 end
 
@@ -88,11 +83,16 @@ end
 ## GET ALL MY FAV EVENTS
 
 get '/favorites/user' do 
-	# binding.pry
 	user = User.find(session['user_id'])
 	events = user.events.to_json
-
 end
+
+delete '/favorites/:id' do 
+	user_id = session["user_id"]
+	find_fav = Favorite.where(:user_id => user_id, :event_id => params[:id])
+	Favorite.destroy(find_fav)
+end
+
 
 
 
